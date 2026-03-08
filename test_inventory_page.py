a7ae76e4951f51743c214f5a37965bf20686783a
Artem <artemshnurkyavichus@gmail.com>
2026-03-08 23:29:29 +0300

    Добавление еще одного теста

---
+++ test_inventory_page.py
@@ -0,0 +1,41 @@
+# test_inventory_page.py
+import sys
+import os
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+from pages.main_page import MainPage
+from pages.inventory_page import InventoryPage
+
+def test_inventory_page_after_login(browser):
+    """Тест проверяет страницу инвентаря после авторизации"""
+    link = "https://www.saucedemo.com/"
+    
+    # Авторизуемся через главную страницу
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.login_as_standard_user()
+    
+    # Ждем загрузки страницы инвентаря
+    WebDriverWait(browser, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "inventory_list"))
+    )
+    
+    # Переходим к странице инвентаря
+    inventory_page = InventoryPage(browser, browser.current_url)
+    
+    # Проверяем страницу инвентаря
+    inventory_page.should_be_inventory_page()
+    inventory_page.should_be_items_present()
+    
+    # Добавляем товар в корзину
+    inventory_page.add_first_item_to_cart()
+    inventory_page.should_be_cart_badge_with_count(1)
+    
+    print("✅ Все проверки страницы инвентаря пройдены")
